4d3727674ec1f1fb45ba3025f4efd6571670e0df
LeoTakachi <leotakachi27@gmail.com>
2026-04-23 14:08:48 -0300

    Fazendo a estrutura base do site

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleciona o botão
+const botao = document.getElementById("btnClick");
+
+// Adiciona evento de clique
+botao.addEventListener("click", function() {
+    alert("Você clicou no botão!");
+});
